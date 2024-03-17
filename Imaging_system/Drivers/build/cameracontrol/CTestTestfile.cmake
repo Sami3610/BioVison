e3d23825aade79d0a5d55ff566d34bf5c383a8a6
sami3610 <sami.giithub@gmail.com>
2024-03-17 19:47:37 +1300

    Your commit message here

---
+++ Imaging_system/Drivers/build/cameracontrol/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aaeon/Desktop/TestCamera/src/cameracontrol
+# Build directory: /home/aaeon/Desktop/TestCamera/build/cameracontrol
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Imaging_system/Drivers/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/aaeon/Desktop/TestCamera/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/aaeon/Desktop/TestCamera/devel;/home/aaeon/PycharmProjects/Image-Capture-System-ICS-/ICS/devel;/opt/ros/noetic'.split(';') if '/home/aaeon/Desktop/TestCamera/devel;/home/aaeon/PycharmProjects/Image-Capture-System-ICS-/ICS/devel;/opt/ros/noetic' != '' else []
